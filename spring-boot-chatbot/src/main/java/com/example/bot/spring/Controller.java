f749f7d2a4981fd8015131d5db9f04034ff26081
VaniOCP <iebardarov@ust.hk>
2017-10-30 16:35:49 +0800

    removed old files

--- spring-boot-chatbot/src/main/java/com/example/bot/spring/Controller.java
+++ spring-boot-chatbot/src/main/java/com/example/bot/spring/Controller.java
@@ -1,6 +1,10 @@
 package src.main.java.com.example.bot.spring;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 
 @Slf4j
 @LineMessageHandler
@@ -9,9 +13,11 @@ public class Controller {
 	@Autowired
 	private LineMessagingClient lineMessagingClient;
 	private MessageHandler messageHandler;
+	private LanguageProcessor languageProcessor;
 	
 	public Controller() {
-		
+		messageHandler = new MessageHandler();
+		languageProcessor = new LuisINLP();
 	}
 	
 	@EventMapping
@@ -20,10 +26,29 @@ public class Controller {
 		log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 		log.info("This is your entry point:");
 		log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-		TextMessageContent message = event.getMessage();
 		
-		messageHandler.handleTextContent(event.getReplyToken(), event, message);
+		String receivedMessage = event.getMessage().getText();
+		
+		//process the message
+		String processedMessage = languageProcessor.processInput(receivedMessage);
+		
+		//get a response from the handler
+		String response = messageHandler.handleTextContent(processedMessage);
+		
+		//send the message back to the user
+		reply(event.getReplyToken(),response);
 	}
 
-	
+	private void reply(@NonNull String replyToken, @NonNull Message message) {
+		reply(replyToken, Collections.singletonList(message));
+	}
+
+	private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+		try {
+			BotApiResponse apiResponse = lineMessagingClient.replyMessage(new ReplyMessage(replyToken, messages)).get();
+			log.info("Sent messages: {}", apiResponse);
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
