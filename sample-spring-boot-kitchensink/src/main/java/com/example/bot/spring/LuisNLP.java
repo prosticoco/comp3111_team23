ad1a65af6e124bccb4a6e2f87b495d4f6d690d6d
VaniOFX <iebardarov@gmail.com>
2017-11-04 01:13:07 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LuisNLP.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LuisNLP.java
@@ -69,7 +69,7 @@ public class LuisNLP implements LanguageProcessor {
 		JSONObject obj = new JSONObject(json);
 		result.add(obj.getJSONObject("topScoringIntent").getString("intent"));
 		if(result.get(0).equals("None") || result.get(0).equals("confirmation")) {
-			log.info(obj.getString("query" + "-------------------------------------------------------------------------"));
+			log.info(obj.getString("query") + "-------------------------------------------------------------------------");
 			result.add(obj.getString("query"));
 		}
 		else {
