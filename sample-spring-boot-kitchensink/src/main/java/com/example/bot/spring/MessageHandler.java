672a082d9562a490c1628626c73e68db06a1e278
VaniOFX <iebardarov@gmail.com>
2017-11-03 23:14:59 +0800

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/MessageHandler.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/MessageHandler.java
@@ -194,7 +194,7 @@ public class MessageHandler {
 
 	private void setTour() throws Exception{
 		if(tour == null && date != null & tour.getId() != null)
-			log.info(tour.getId() + date.toString());
+			log.info(tour.getId() + date.toString() + "---------------------------------------------------------------------");
 			tour = database.getTourDetails(tour.getId(), date);
 	}
 
