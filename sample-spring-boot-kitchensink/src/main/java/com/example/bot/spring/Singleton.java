8d31f3d884e7f183c8dd0ac30e657b790dd100ff
ChrisLynch96 <chlynch@tcd.ie>
2017-11-03 16:45:15 +0800

    Implemented greeting response in message hadler and wrote greeting test for Luis

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Singleton.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/Singleton.java
@@ -1,3 +1,4 @@
+
 /*
 import src.main.java.com.example.bot.spring.Customer;
 import src.main.java.com.example.bot.spring.Tour;
@@ -34,3 +35,40 @@ public class Singleton {
 	
 }
 */
+=======
+//import java.util.ArrayList;
+//
+//import com.example.bot.spring.Customer;
+//import com.example.bot.spring.Tour;
+//
+//// Oliver Elleman Thomsen
+//public class Singleton {
+//	
+//	private Singleton() {};
+//	
+//	private static Singleton uniqueInstance = new Singleton();
+//	private Customer customer;
+//	private ArrayList<Tour> tours;
+//	
+//	
+//	public static Singleton getInstance() {
+//		return uniqueInstance;
+//	}
+//	
+//	public void newCustomer() {
+//		//TODO: instantiate customer object
+//	}
+//	public void newTour() {
+//		//TODO: instantiate tour and add to arrayList
+//	}
+//	
+//
+//	public Customer getCustomer() {
+//		return customer;
+//	}
+//
+//	public ArrayList<Tour> getTours() {
+//		return tours;
+//	}
+//	
+//}
