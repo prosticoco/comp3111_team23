e09f28234ba20de95f26ee7b4ef6889a83d10d84
OliverThomsen <s154023@student.dtu.dk>
2017-11-20 19:28:13 +0800

    refactored controller into 3 classes

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LineCommunicator.java
@@ -0,0 +1,24 @@
+package src.main.java.com.example.bot.spring;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Oliver Thomsen
+ *
+ */
+public class LineCommunicator implements Communicator{
+
+	@Override
+	public void replyText(String replyToken, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pushCustomerNotification(ArrayList<String> recepients, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
